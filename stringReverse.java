f1f37baa7ffcfae027eb2ba969b4209d7577cdc9
shauntheawesome1 <Shaun.munshi@gmail.com>
2018-10-07 13:05:26 -0700

    Create stringReverse.java

---
+++ stringReverse.java
@@ -0,0 +1,24 @@
+package com.ngtlearning;
+
+import java.util.Scanner;
+
+
+public class WordReverse {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter any sentence : ");
+		String userInput = s.nextLine();
+		
+		WordReverse myObject = new WordReverse();
+		System.out.println(myObject.WordReverse(userInput));
+		s.close();
+
+	}
+
+	private String [] WordReverse(String binNumber) {
+		
+		return null;
+	}
+
+}
